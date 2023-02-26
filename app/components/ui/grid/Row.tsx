c55dc6d677d6225389daf5ac2ae7a002d7c62c05
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-26 15:29:42 +0300

    fix: styles

--- app/components/ui/grid/Row.tsx
+++ app/components/ui/grid/Row.tsx
@@ -3,9 +3,11 @@ import { FC, PropsWithChildren } from 'react'
 
 
 
-const Row: FC<PropsWithChildren<{ className?: string }>> = ({ children, className }) => {
+const Row: FC<PropsWithChildren<{ className?: string, isBorder?: boolean }>> = ({ children, className, isBorder }) => {
   return (
-    <div className={cn('grid grid-cols-12 w-full border-b-2 border-black', className)}>
+    <div className={cn('grid grid-cols-12 w-full', {
+      'border-b-2 border-black border-solid': isBorder
+    }, className)}>
       {children}
     </div>
   )
