5ed957d04edfe8b7e81bb1f172ea80098353de08
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-26 14:18:29 +0300

    feat: add layout

--- app/components/layout/header/buttons/HeaderButtons.tsx
+++ app/components/layout/header/buttons/HeaderButtons.tsx
@@ -1,10 +1,19 @@
-import {FC} from 'react'
+import Column from '@/ui/grid/Column'
+import SquareButton from '@/ui/square-button/SquareButton'
+import { FC } from 'react'
+import { FiSearch } from 'react-icons/fi'
+import { FiBell } from 'react-icons/fi'
+import { FiShoppingCart } from 'react-icons/fi'
 
 
 
 const HeaderButtons: FC = () => {
   return (
-    <div>HeaderButtons</div>
+    <Column size={2} className='gap-5'>
+      <SquareButton Icon={FiSearch} onClick={()=>{}}/>
+      <SquareButton Icon={FiBell} onClick={()=>{}}/>
+      <SquareButton Icon={FiShoppingCart} onClick={()=>{}} number={5}/>
+    </Column>
   )
 }
 
