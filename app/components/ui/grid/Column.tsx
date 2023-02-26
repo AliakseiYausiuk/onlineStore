776486e939c42965784c21bd23e98280fb4cf057
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-26 14:20:27 +0300

    feat: add interface and update styles

--- app/components/ui/grid/Column.tsx
+++ app/components/ui/grid/Column.tsx
@@ -1,12 +1,25 @@
 import { FC, PropsWithChildren } from 'react'
 import cn from 'clsx'
 
+interface IColumn {
+  size: number
+  isCenter?: boolean
+  className?: string
+  isPadding?: boolean
+}
 
-const Column: FC<PropsWithChildren<{ size: number, isCenter?: boolean }>> = ({ size, children, isCenter = true }) => {
+
+const Column: FC<PropsWithChildren<IColumn>> = ({ size, children, isCenter = true, className, isPadding = true }) => {
   return (
     <div
       style={{ gridColumn: `span ${size} / span ${size}` }}
-      className={cn('border-r-2 border-black border-solid h-full flex items-center p-3',{'justify-center': isCenter})}
+      className={cn(
+        'border-r-2 border-black border-solid h-full flex items-center',
+        {
+          'justify-center': isCenter,
+          'p-3': isPadding
+        },
+        className)}
     >
       {children}
     </div>
