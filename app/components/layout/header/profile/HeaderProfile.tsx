a673524f76ec37e3d98ba36f0b6c1b272cce0507
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-26 14:19:20 +0300

    feat: add layout

--- app/components/layout/header/profile/HeaderProfile.tsx
+++ app/components/layout/header/profile/HeaderProfile.tsx
@@ -1,10 +1,18 @@
-import {FC} from 'react'
+import Column from '@/ui/grid/Column'
+import SquareButton from '@/ui/square-button/SquareButton'
+import { FC } from 'react'
+import { FiUser } from 'react-icons/fi'
 
 
 
-const HeaderProfile:FC = () => {
+const HeaderProfile: FC = () => {
   return (
-    <div>HeaderProfile</div>
+    <Column size={3} className='flex items-center'>
+      <SquareButton Icon={FiUser}/>
+      <div className='ml-3'>
+        <div className='text-gray text-sm'>Aliaksei Yausiuk</div>
+      </div>
+    </Column>
   )
 }
 
