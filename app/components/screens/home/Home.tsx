2321b06ef12e6c7c2790883bae40ea045203b0e9
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-26 14:19:38 +0300

    feat: layout

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -3,15 +3,23 @@ import { FC } from 'react'
 import Layout from '@/layout/Layout'
 
 import { IProductsPage } from '@/../pages'
-import Loader from '@/ui/loader/Loader'
+import Row from '@/ui/grid/Row'
+import Column from '@/ui/grid/Column'
+import Image from 'next/image'
+
 
 const Home: FC<IProductsPage> = ({ products }) => {
 	return (
 		<Layout
 			title='Home'
-			description='More than just great coffee. Explore the menu, sign up for Starbucks® Rewards, manage your gift card and more.'
+			description='More than just great coffee. Explore the menu, 
+			sign up for Starbucks® Rewards, manage your gift card and more.'
 		>
-			<Loader/>
+			<Row className='bg-dark-primary'>
+				<Column size={7} isPadding={false}>
+					<Image src='/images/main-4x.png' alt='' width={720} height={628}/>
+				</Column>
+			</Row>
 		</Layout>
 	)
 }
