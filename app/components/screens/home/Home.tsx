9b1a168f85faafe511ee3315d00f61d5591b9b36
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-03-11 13:03:52 +0300

    fix: add texture

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -17,9 +17,12 @@ const Home: FC<IProductsPage> = ({ products }) => {
 			description='More than just great coffee. Explore the menu, 
 			sign up for Starbucks® Rewards, manage your gift card and more.'
 		>
-			<Row className='bg-dark-primary'>
+			<Row className='bg-dark-primary' style={{
+				backgroundBlendMode: 'multiply',
+				backgroundImage: 'url(/images/texture.jpg)'
+			}}>
 				<Column size={7} isPadding={false}>
-					<Image src='/images/main-4x.png' alt='' width={720} height={628}/>
+					<Image src='/images/main-4x.png' alt='' width={833} height={727}/>
 				</Column>
 				<Column size={5} isCenter={false} className='flex-col justify-between' isPadding={false}>
 					<HotSale/>
